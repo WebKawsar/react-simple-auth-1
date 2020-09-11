dd72eff7c9211c38f565ea49d1f2c54d4c1e7ca3
Web Kawsar <web.kawsarahmed@gmail.com>
2020-09-11 20:11:07 +0600

    net and clean

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from './firebase.config';
 
 
 
 
-firebase.initializeApp(firebaseConfig)
 
 function App() {
 
 
   return (
-    <div className="App">
-      
+    <div>
+
     </div>
   );
 }
